ad1e642c3ee371ba04316a277977a08d7814a0c9
Takerlin Gereidi <Takerlin Gereidi@DESKTOP-15BE5S5>
2022-09-13 15:16:27 -0700

    Agregar archivos de proyecto.

---
+++ Practica2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad = 20;
+            string Nombre = "Takerlin";
+            string Pais = "Alemania";
+
+            Console.WriteLine("Ayola mi nombre es: {0}, tengo {1} años de edad y me gustaria conocer  {2}", Edad, Nombre, Pais);
+            Console.ReadKey();
+        }
+    }
+}
+
+    
